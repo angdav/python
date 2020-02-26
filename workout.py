535a848aae373e2c770b6a77f4dccefb2763bff1
David Ang <cdang1024@gmail.com>
2020-02-25 23:48:50 -0500

    added timestamps and stats

--- workout.py
+++ workout.py
@@ -1,3 +1,11 @@
+# cli program for tracking workouts
+
+import datetime
+
+lines = ["", "", "", "\n"]
+
+start = datetime.datetime.now()
+
 outf = open("workout.txt", 'w')
 
 pushCount = 0
@@ -24,12 +32,32 @@ while True:
         print("invalid exercise")
         continue
     
-    outf.write(' '.join(i)+"\n")
+    lines.append(datetime.datetime.now().strftime("[%H:%M:%S] ") + ' '.join(i)+"\n")
 
-outf.write("\nTotal:\n")
-outf.write(str(pushCount) + " pushups\n")
-outf.write(str(pullCount) + " pullups\n")
-outf.write(str(squatCount) + " squats\n")
-outf.write(str(jackCount) + " jumping jacks\n\n")
+end = datetime.datetime.now()
+duration = str(end - start)
+duration = duration[:duration.rfind('.')]
+
+totalreps = pushCount+pullCount+squatCount+jackCount
+rpm = totalreps/((end-start).total_seconds()/60) # reps per minute
+rps = (end-start).total_seconds()/(len(lines)-4) # rest per set
+
+lines[0] = "Start:\t" + start.strftime("%Y-%m-%d %H:%M:%S") + "\n"
+lines[1] = "End:\t" + end.strftime("%Y-%m-%d %H:%M:%S") + "\n"
+lines[2] = "Duration: " + duration + "\n"
+
+lines.append("\nTotal:\n")
+lines.append(str(pushCount) + "\tpushups\n")
+lines.append(str(pullCount) + "\tpullups\n")
+lines.append(str(squatCount) + "\tsquats\n")
+lines.append(str(jackCount) + "\tjumping jacks\n")
+
+lines.append("\nStats:\n")
+lines.append(str(pushCount+pullCount+squatCount+jackCount) + "\ttotal reps\n")
+lines.append(str(int(rpm)) + "\treps per minute\n")
+lines.append(str(int(rps)) + "\tseconds between sets on average\n")
+
+for line in lines:
+    outf.write(line)
 
 outf.close()
